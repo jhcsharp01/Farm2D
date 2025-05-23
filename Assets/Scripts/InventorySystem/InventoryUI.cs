1d6ed6caa703e2f0868f21516e6f901224d9c85d
YoungMin Cho (Luke) <makebaram@gmail.com>
2025-05-23 10:53:29 +0900

    2025-05-23
    2025-05-23

---
+++ Assets/Scripts/InventorySystem/InventoryUI.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.InventorySystem
+{
+    public class InventoryUI : MonoBehaviour
+    {
+        public GameObject inventory; //인벤토리 창
+
+        public Player player; //플레이어 등록
+
+        public List<SlotUI> slots = new List<SlotUI>(); //슬롯 UI 묶음
+
+        private void Update()
+        {
+            //버튼 누르면 인벤토리 키는 기능
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                OnOFF();
+            }
+        }
+
+        private void OnOFF()
+        {
+            //켜짐 여부에 따라 true와 false로 인벤토리를 키거나 끕니다.
+            if(inventory.activeSelf)
+            {
+                inventory.SetActive(false);
+                //켯을 경우 UI에 대한 갱신 진행
+                SlotRenewal();
+            }
+            else
+            {
+                inventory.SetActive(true);
+            }
+        }
+
+        //슬롯에 대한 갱신
+        private void SlotRenewal()
+        {
+            if (slots.Count == player.Inventory.slots.Count)
+            {
+                for(int i = 0; i < slots.Count; i++)
+                {
+                    if (player.Inventory.slots[i].type != CollectType.None)
+                    {
+                        //슬롯에 이미지와 개수 등을 갱신한다.
+                    }
+                }
+
+            }
+
+        }
+    }
+}
+
